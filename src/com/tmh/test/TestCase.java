824a36478635fcf9b7935a4b71ae4bd25ffd9d4b
TangMinghao <tmhxwq@163.com>
2016-12-21 22:05:43 +0800

    fix:第一个提交

---
+++ src/com/tmh/test/TestCase.java
@@ -0,0 +1,10 @@
+package com.tmh.test;
+
+/**
+ * Created by Tang Minghao on 2016/12/21.
+ */
+public class TestCase {
+    public static void main(String [] args){
+        System.out.println("Git start coding");
+    }
+}
